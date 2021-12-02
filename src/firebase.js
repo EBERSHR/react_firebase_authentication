6aa1f5f872438c7b356923621cce4becd59cfe92
Eber Hernández <ebershr@gmail.com>
2021-12-02 17:36:18 -0500

    Prueba de deploy

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBfgvTXGhWSiEsN73IjhwnU7GMnULdwFtA",
+    authDomain: "reactfirebaseauthenticat-ee532.firebaseapp.com",
+    projectId: "reactfirebaseauthenticat-ee532",
+    storageBucket: "reactfirebaseauthenticat-ee532.appspot.com",
+    messagingSenderId: "530473542286",
+    appId: "1:530473542286:web:8b84a35185e7622382dfdf"
+  };
